ae210e71e313b670cbaccd148ef0feb7dbff685a
IESG-Eldoritoskun <ignacio06777@gmail.com>
2026-03-19 13:35:20 -0600

    Este es mi primer commit para github

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar';
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <div>
+      Hello, Vite + React!
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+        <nav>
+            <h1>Este es mi Navbar</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
